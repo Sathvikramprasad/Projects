c6ed18f3fac269bb273b63ee5a3ef1c87a5cdff7
Sathvikramprasad <sxr3949@mavs.uta.edu>
2025-01-05 17:30:35 -0600

    push with modifications

--- Clock(C++)/main.cpp
+++ Clock(C++)/main.cpp
@@ -1,4 +1,4 @@
-#include "clock.h"
+#include "timer.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -8,18 +8,20 @@ int main(int argc, char* argv[]) {
     }
     std::string input;
     try {
-        Clock clock{atoi(argv[1]), atoi(argv[2]), atoi(argv[3])};
+        Timer clock{atoi(argv[1]), atoi(argv[2]), atoi(argv[3])};
         std::cout << "\nEnter 'q' to quit.\n" << std::endl;
         while(input != "q") {
-            std::cout << "The time is now ";
+            std::cout << "Remaining time is now ";
             clock.print();
             std::getline(std::cin, input);
             clock.tic();
         }
+    } catch (std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
     } catch (std::out_of_range& e) {
         std::cerr << e.what() << std::endl;
-        std::cin.clear();            // clear any input errors
-        std::cin.ignore(2048, '\n'); // clear input buffer - technically should be
+        std::cin.clear();
+        std::cin.ignore(2048, '\n'); // clear input line - technically should be
         // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         // std::getline(std::cin, input); // will do the same thing
         return -2; // or exit(-2)
